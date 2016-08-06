a8fca58a0503e0e22ac2a65ec28fe57defa18b30
patrickliamhartley <patrickliamhartley@gmail.com>
2016-08-06 14:19:37 -0700

    Implement basic functioning collision

--- client/watchout.js
+++ client/watchout.js
@@ -34,16 +34,37 @@ for (var i = 0; i < 10; i++) {
 }
 
 var currentScore = 0;
+var highscore = 0;
 
 var scoring = function() {
   currentScore++;
   d3.selectAll('.current').select('span').text(currentScore);
 };
 
+var reset = function() {
+  if (currentScore > highscore) {
+    highscore = currentScore;
+  }
+  currentScore = 0;
+  d3.selectAll('.highscore').select('span').text(highscore);
+  d3.selectAll('.current').select('span').text(currentScore);
+};
+
+var collisions = 0;
+
+
+var collide = function() {
+  collisions++;
+  d3.selectAll('.collisions').select('span').text(collisions);
+};
+
+
+
 var createPlayer = function() {
 
   gameboard.data(player)
     .append('circle')
+    .attr('class','player')
     .attr('r', 15)
     .attr('cx', function(d) { return d.x; })
     .attr('cy', function(d) { return d.y; })
@@ -53,23 +74,6 @@ var createPlayer = function() {
       .on("end", dragended));
 };
 
-
-//collisions
-// var simulation = d3.forceSimulation([player,asteroids])
-//     .velocityDecay(0.2)
-//     .force("x", d3.forceX().strength(0.002))
-//     .force("y", d3.forceY().strength(0.002))
-//     .force("collide", d3.forceCollide(10))
-//     .on("tick", ticked);
-
-
-
-
-
-///drag and drop controls///
-var ticked = function(){
-  console.log("collision detected");
-};
 
 var dragstarted = function(d) {
   d3.select(this).classed("dragging", true);
@@ -83,6 +87,29 @@ var dragended = function(d) {
   d3.select(this).classed("dragging", false);
 };
 
+// function isPointInPoly(poly, pt){
+//   console.log(poly, pt);
+//     for(var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+//         ((poly[i].__data__.y <= pt.y && pt.y < poly[j].__data__.y) || (poly[j].__data__.y <= pt.y && pt.y < poly[i].__data__.y))
+//         && (pt.x < (poly[j].__data__.x - poly[i].__data__.x) * (pt.y - poly[i].__data__.y) / (poly[j].__data__.y - poly[i].__data__.y) + poly[i].__data__.x)
+//         && (c = !c);
+//     return c;
+// }
+
+var colDetect = function(ast, pl, padding) {
+  var pad = 2 * padding;
+  var test = false;
+  for (var i = 0; i < ast.length - 1; i ++) {
+    console.log(pl.x, ast[i].__data__.x, pl.y, ast[i].__data__.y);
+    if (((pl.x <= ast[i].__data__.x + pad) && (pl.x >= ast[i].__data__.x - pad)) && (
+      (pl.y <= ast[i].__data__.y + pad) && (pl.y >= ast[i].__data__.y - pad))) {
+        console.log("true");
+        return true;
+    
+    }
+  }
+  return test;
+};
 
 
 var update = function(asteroids) {
@@ -94,32 +121,48 @@ var update = function(asteroids) {
 
   selection
     .transition(trans)
-    .attr('x', function(d) { d.changeLocation(); return d.x; })
-    .attr('y', function(d) { return d.y; });
+    .attr('cx', function(d) { d.changeLocation(); return d.x; })
+    .attr('cy', function(d) { return d.y; });
+    // .on("mouseenter",function(){
+    //   console.log("mouseover");
+    //   collide();
+    // });
+
+
 
 
   selection.enter()
-    .append("svg:image")
+    .append("circle")
     .attr("class", "asteroids")
-    .attr('width', 30)
-    .attr('height', 30)
-    .attr('r', 20)
+    // .attr('width', 30)
+    // .attr('height', 30)
+    .attr('r', 15)
     .attr('xlink:href', 'asteroid.png')
-    .attr("x", function(d) { return d.x; })
-    .attr("y", function(d) { return d.y; });
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; });
     
 
   selection.exit()
     .remove();
 };
 
+
+
+
+var selection = gameboard.selectAll(".asteroids").data(asteroids, function(d) { return d.id; });
 update(asteroids);
 createPlayer();
+//isPointInPoly(selection._enter, player[0]);
 d3.interval(function() {
   scoring();
+  if (colDetect(selection._enter[0], player[0],15)) {
+    reset();
+    collide();
+  }
+  //console.log(isPointInPoly(selection.attr('x'), player[0]));
 }, 50);
 d3.interval(function() {
   console.log("updating");
   update(asteroids);
-}, 1000);
+}, 2000);
 
